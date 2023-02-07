151b4a333d690962c233432ba2757d9cdf169b2a
Saddam <saddam13@cse.pstu.ac.bd>
2023-02-07 17:46:35 +0600

    basic backend server for dragon news

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+
+app.get('/', (req, res)=>{
+    res.send('News API Running');
+});
+
+app.get('/news-categories', (req, res) =>{
+    res.send(categories);
+})
+
+app.listen(port, ()=>{
+    console.log('Dragon News server running on port ', port);
+})
